ae6c482c7fe6123f9f71003b06119456a564faee
Adrianna <guevarra@oregonstate.edu>
2019-04-18 16:17:59 -0700

    Defined book Constructor

---
+++ src/edu/osucascades/guevarra/Book.java
@@ -0,0 +1,40 @@
+package edu.osucascades.guevarra;
+
+public class Book {
+    String title;
+    boolean borrowed;
+
+    // Creates a new Book
+    public Book(String bookTitle) {
+        title = bookTitle;
+    } // Implement this method }
+
+    // Marks the book as rented
+    public void borrowed() {
+
+    }
+
+    // Implement this method }
+    //Marks the book as not rented
+    public void returned() {
+
+    }
+
+    //Implement this method
+    // Returns true if the book is rented, false otherwise
+    public boolean isBorrowed() { // Implement this method }
+        // Returns the title of the book
+        public String getTitle() {
+        }
+        //Implement this method }
+    }
+
+    public static void main(String[] args) {
+        Book example = new Book("The Da Vinci Code");
+        System.out.println("Title(should be The Da Vinci Code);" + example.isBorrowed());
+        example.rented();
+        System.out.println("Borrowed? (should be true):" + example.isBorrowed()):example.returned();
+        System.out.println("Borrowed? (should be false):"_example.isBorrowed()):
+    }
+
+}
