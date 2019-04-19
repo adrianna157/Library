2d63a798d685bb448b07c7f0c805aa90b3072561
Adrianna <guevarra@oregonstate.edu>
2019-04-19 16:51:22 -0700

    Added Library Class

--- src/edu/osucascades/guevarra/Book.java
+++ src/edu/osucascades/guevarra/Book.java
@@ -10,31 +10,38 @@ public class Book {
     } // Implement this method }
 
     // Marks the book as rented
-    public void borrowed() {
-
+    public boolean borrowed() {
+        borrowed = false;
+        return borrowed;
     }
 
     // Implement this method }
     //Marks the book as not rented
     public void returned() {
+        borrowed = true;
 
     }
 
     //Implement this method
     // Returns true if the book is rented, false otherwise
-    public boolean isBorrowed() { // Implement this method }
-        // Returns the title of the book
-        public String getTitle() {
-        }
-        //Implement this method }
+    public boolean isBorrowed() {
+        return borrowed;
+    }// Implement this method }
+
+    // Returns the title of the book
+    public String getTitle() {
+        return title;
     }
+    //Implement this method }
+
 
     public static void main(String[] args) {
         Book example = new Book("The Da Vinci Code");
-        System.out.println("Title(should be The Da Vinci Code);" + example.isBorrowed());
-        example.rented();
-        System.out.println("Borrowed? (should be true):" + example.isBorrowed()):example.returned();
-        System.out.println("Borrowed? (should be false):"_example.isBorrowed()):
+        System.out.println("The Da Vinci Code;" + example.isBorrowed());
+        example.borrowed();
+        System.out.println("Borrowed? (should be true):" + example.isBorrowed());
+        example.returned();
+        System.out.println("Borrowed? (should be false):" + example.isBorrowed());
     }
 
 }
